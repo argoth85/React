923185d84c174a5f74a0e38277d8572c8560dded
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-05 16:31:59 +0200

    exercise 02

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
+
+// ****** soluzione 01 function ******
+
+const sayHello = (name) => <h1>Hello, {name}</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    {sayHello('Vincenzo')}
   </React.StrictMode>
 );
+
+
+// ****** soluzione 02 variabile ******
+
+
+// let name = 'Vincenzo';
+// const hello = <h1>Hello, {name}</h1>;
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {hello}
+//   </React.StrictMode>
+// );
