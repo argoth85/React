6397b508835957c9662666f81c5cdf957579f8e3
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-05 16:14:31 +0200

    exercise 01

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+let hello = <h1>Hello, World!</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    {hello}
   </React.StrictMode>
 );
