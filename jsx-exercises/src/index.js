e792e84acc0bcc99db0a047195a04d43b2438c08
Argoth <argoth@MacBook-Pro-di-Argoth.local>
2022-07-05 16:44:45 +0200

    exercise 03

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
+
+// ***** metodo 01 *****
+
+// const sum = (a, b) => <h2>{a + b}</h2>;
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {sum(3, 7)}
+//   </React.StrictMode>
+// );
+
+
+
+// ***** metodo 02 *****
+
+const somma = (a, b) => a + b;
+const result = <h2>The result is {somma(1,5)}</h2>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    {result}
   </React.StrictMode>
 );
